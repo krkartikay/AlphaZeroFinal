655872a5dbc5c4c0735b133bed1485e99115421c
Kumar Kartikay <krkartikay@gmail.com>
2020-12-12 18:40:05 +0530

    server  done

--- server.py
+++ server.py
@@ -5,15 +5,31 @@
 #  - POST /train New training data
 
 # Communicates with training process (train.py) via files:
-#  - latest_model.h5 : Latest model weights
+#  - latest_weights.h5 : Latest model weights
 #  - training_data.log : Training data sent by clients
 
 import flask
+import io
+from filelock import FileLock
 
 app = flask.Flask(__name__)
 
-@app.route("/hi")
-def say_hi():
-    return "Hello!"
+@app.route("/weights", methods=("GET",))
+def get_weights():
+    # send file 'latest_weights.h5'
+    with FileLock("latest_weights.h5.lock"):
+        # return flask.send_file(io.BytesIO(open("latest_weights.h5", "rb").read()),
+        #                         attachment_filename="latest_weights.h5",
+        #                         mimetype="application/octet-stream")
+        return open("latest_weights.h5", "rb").read()
 
-app.run(port=5000)
+@app.route("/train", methods=("POST",))
+def post_training_data():
+    # get training data from client and append to file 'training_data.log' atomically
+    # data = request.something ... see flask docs
+    with FileLock("training_data.log.lock"):
+        with open("training_data.log", "ab") as file:
+            file.write(flask.request.data)
+    return flask.jsonify("OK")
+
+app.run(port=5000, debug=True)
